17bccb70f30085bdda215ae746138e942979f464
danielhleducastur <danielhl@educastur.org>
2024-01-15 14:16:04 +0100

    Update ArcadeKartPowerup.cs

--- Assets/Karting/Scripts/ArcadeKartPowerup.cs
+++ Assets/Karting/Scripts/ArcadeKartPowerup.cs
@@ -1,7 +1,7 @@
 ﻿using KartGame.KartSystems;
 using UnityEngine;
 using UnityEngine.Events;
-
+//cambio de la rama1
 public class ArcadeKartPowerup : MonoBehaviour {
 
     public ArcadeKart.StatPowerup boostStats = new ArcadeKart.StatPowerup
